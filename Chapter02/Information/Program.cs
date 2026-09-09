0f9080dd11d6f59440f025c367a623eddbfb6923
Wert3x <valdismen25@gmail.com>
2026-09-09 14:33:59 +0300

    Made a project named Information to practice with variables.

---
+++ Chapter02/Information/Program.cs
@@ -0,0 +1,11 @@
+﻿string bookTitle = "C# Programming";
+int pages = 500;
+decimal price = 49.99m;
+bool hasDiscount = false;
+double rating = 4.9;
+
+System.Console.WriteLine($"Книга: {bookTitle}");
+System.Console.WriteLine($"Количество страниц: {pages}");
+System.Console.WriteLine($"Цена: {price}");
+System.Console.WriteLine($"Скидка: {hasDiscount}");
+System.Console.WriteLine($"Рейтинг: {rating}");
