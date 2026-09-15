15954bebe11d425ec61ee34225bc05532a4601a6
Wert3x <valdismen25@gmail.com>
2026-09-16 01:19:29 +0300

    Learned how to store any type of object.

---
+++ Chapter02/Storing-Any-Type-Of-Object/Program.cs
@@ -0,0 +1,15 @@
+﻿object[] objects = {"Vlad", 67, false, 9.81};
+
+foreach(object item in objects)
+{
+    System.Console.Write($"Type: {item.GetType().Name}, value: {item}. ");    
+
+    if (item is string s)
+        System.Console.WriteLine($"Length: {s.Length}");
+    else if (item is int i)
+        System.Console.WriteLine($"Doubled: {i * 2}");
+    else if (item is bool b)
+        System.Console.WriteLine($"Negatived: {!b}");
+    else if (item is double d)
+        System.Console.WriteLine($"With 3 decimal places: {d:F3}");
+}
--- Chapter02/Storing-Multiple-Values-In-An-Array/Program.cs
+++ Chapter02/Storing-Multiple-Values-In-An-Array/Program.cs
@@ -1,7 +1,4 @@
-﻿using System.Globalization;
-using System.Net;
-
-int[] numbers = new int[5];
+﻿int[] numbers = new int[5];
 
 for (int i = 0; i < 5; i++)
 {
