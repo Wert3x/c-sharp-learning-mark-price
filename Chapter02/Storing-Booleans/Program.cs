2df72b756278cfeb7e0904feb2385b7681e1ac1f
Wert3x <valdismen25@gmail.com>
2026-09-15 21:11:43 +0300

    Made a solution to practice with storing booleans and made a practice task.

---
+++ Chapter02/Storing-Booleans/Program.cs
@@ -0,0 +1,17 @@
+﻿System.Console.Write("Your height(m): ");
+double height = double.Parse(System.Console.ReadLine());
+
+System.Console.Write("Your weight(kg): ");
+double weight = double.Parse(System.Console.ReadLine());
+
+double bodyMassIndex = weight / (height * height);
+
+string category = bodyMassIndex switch
+{
+    < 18.5 => "Underweight",
+    < 25 => "Normal",
+    < 30 => "Overweight",
+    _ => "Obesity"
+};
+
+System.Console.WriteLine($"Your category: {category}");
