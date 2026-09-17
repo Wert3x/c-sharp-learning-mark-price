114b6bb937e5ac42172a31446462de62b287d9d0
Wert3x <valdismen25@gmail.com>
2026-09-17 17:42:24 +0300

    Learned and practiced about dynamic type and storing it.

---
+++ Chapter02/Storing-Dynamic-Types/Program.cs
@@ -0,0 +1,17 @@
+﻿dynamic something;
+
+something = new[] {1, 2, 3, 4};
+try { System.Console.WriteLine(something.Length); }
+catch (Exception ex) {System.Console.WriteLine($"Error: {ex.Message}");}
+
+something = "Avada kedavra";
+try { System.Console.WriteLine(something.Length); }
+catch (Exception ex) {System.Console.WriteLine($"Error: {ex.Message}");}
+
+something = 12334;
+try {System.Console.WriteLine(something.Length);}
+catch (Exception ex) {System.Console.WriteLine($"Error: {ex.Message}");}
+
+something = true;
+try {System.Console.WriteLine(something.Length);}
+catch (Exception ex) {System.Console.WriteLine($"Error: {ex.Message}");}
